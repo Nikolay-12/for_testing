b2c53175b5262ff648b3fad789a36597f02dd2d0
Nikolay-12 <104552191+Nikolay-12@users.noreply.github.com>
2025-09-21 17:02:43 +0300

    Update test.h

--- test.h
+++ test.h
@@ -1,14 +1,24 @@
 #pragma once
 
-#define num 1.2
-#define num 5.0
+#define NANO_CLK_PIN 13 // Пин SCK термопар
+#define NANO_MISO_PIN 12 // ПИН MISO термопар
+#define HC595_DS_PIN 2 // Пин данных для сдвиговых регистов ("путешествующий ноль")
+#define HC595_STcp_PIN 3 // Пин для защелкивания выходов сдвиговых регистов
+#define HC595_SHcp_PIN 4 // Пин тактовых импульсов для сдвиговых регистов
 
-namespace space1
+namespace 74HC595
 {
-  void func(num1, num2);
+  void setting_up_reg_pins();
+  void set_bit_on_DS_pin(bool bit);
+  void shift_bit_in_reg();
+  void save_data_in_reg();
+  void set_11111111_in_reg();
+  void add_0_to_reg();
+  void shift_and_save_0_in_reg();
 }
 
-namespace space2
+namespace MAX6675
 {
-  void func(num1, num2);
+  void setting_pin_values_for_SPI();
+  double readCelsius();
 }
